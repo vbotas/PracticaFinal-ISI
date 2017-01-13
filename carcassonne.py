e4f0287ed5c2e0163a7105cc078393e20fc56888
vbotas <victorbotasmenendez@gmail.com>
2017-01-13 05:12:44 -0500

    Clase para crear las piezas del territorio de manera aleatorio + test de tamaño pieza

--- carcassonne.py
+++ carcassonne.py
@@ -1,3 +1,5 @@
+import random
+
 class Partida(object):
     jugadores = 2
     piezas_territorio = 72
@@ -14,4 +16,25 @@ class Jugador (object):
     meeples = 8
     def __init__(self):
         pass
+
+class Pieza_Territorio(object):
+    pieza = [0,0,0,0]
+    def __init__(self):
+        parte_pieza = 0
+        while parte_pieza < len(self.pieza):
+            num_aleat = random.randint(0,4)
+            if num_aleat >= 0 and num_aleat < 1:
+                self.pieza[parte_pieza] = "Monasterio"
+                parte_pieza+=1
+            elif num_aleat >= 1 and num_aleat < 2:
+                self.pieza[parte_pieza] = "Castillo"
+                parte_pieza+=1
+            elif num_aleat >= 2 and num_aleat < 3:
+                self.pieza[parte_pieza] = "Camino"
+                parte_pieza+=1
+            else:
+                self.pieza[parte_pieza] = "Granja"
+                parte_pieza+=1
+        print (self.pieza)
+        
 
--- carcassonne_test.py
+++ carcassonne_test.py
@@ -16,17 +16,26 @@ class Test_basico(unittest.TestCase):
         self.assertEqual(jug2, 'juan', msg="Algo ha fallado al crear al jugador 2")
         
     def test_numero_jugadores (self):
+        ##test para comprobar que el número de jugadores no es mayor a 4
         numero_jugadores = self.partida.jugadores
         self.assertLessEqual(numero_jugadores, 4, msg="Mas jugadores de los permitidos.") 
         
     def test_piezas_territorio(self):
+        ##test para comprobar que el numero de piezas de territorio es el correcto, es decir, 72
         numero_piezas_territorio = self.partida.piezas_territorio
         self.assertEqual(numero_piezas_territorio, 72, msg="No hay las piezas de territorio necesarias para comenzar")
     
     def test_meeples_jugadores(self):
+        ##test para comprobar que el numero de meeples es correcto al no ser superior a 8
         meeples_jugador = self.jugador.meeples
         self.assertLessEqual(meeples_jugador, 8, msg="El numero de meeples por jugador no es correcto")
         
     def test_marcador_jugador(self):
+        ## test para comprobar que el marcador no es negativo.
         marcador_jug = self.jugador.marcador
-        self.assertEqual(marcador_jug, 0, msg="Marcador incorrecto")
+        self.assertGreaterEqual(marcador_jug, 0, msg="Marcador incorrecto")
+        
+    def test_pieza(self):
+        ##test para comprobar que la pieza tiene las partes necesarias.
+        prueba_pieza = Pieza_Territorio()
+        self.assertEqual(len(prueba_pieza.pieza), 4, msg="Fallo al crear la pieza")
