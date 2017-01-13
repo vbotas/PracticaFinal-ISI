2f7ae50e7fb1ff3287d72dfcec03beebfc1d3602
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2017-01-14 00:16:35 +0100

    Añadida funcionalidad para las coordenadas

--- carcassonne.py
+++ carcassonne.py
@@ -53,7 +53,7 @@ class Pieza_terreno:
         elif tipo == 7:
             posicion = ['Granja','Camino','Camino','Camino']
         elif tipo == 8:
-            posicion = ['Granja','Granja','Granja','Granja']
+            posicion = ['Granja','Granja','Granja','Granja']  ## ESTA FIGURA TIENE UN MONASTERIO EN EL CENTRO
         elif tipo == 9:
             posicion = ['Granja','Castillo','Granja','Castillo']
         elif tipo == 10:
@@ -71,7 +71,7 @@ class Pieza_terreno:
         elif tipo == 16:
             posicion = ['Granja','Castillo','Castillo','Granja']
         elif tipo == 17:
-            posicion = ['Granja','Granja','Camino','Granja']
+            posicion = ['Granja','Granja','Camino','Granja']  ## ESTA FIGURA TIENE UN MONASTERIO EN EL CENTRO
         elif tipo == 18:
             posicion = ['Camino','Camino','Camino','Camino']
         elif tipo == 19:
@@ -81,3 +81,4 @@ class Pieza_terreno:
     # Inicializa la clase pieza territorio
     def __init__(self, tipo):
         self.posicion = self.asignar_posicion(tipo)
+        self.coordenadas = [None,None]
