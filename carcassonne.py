4a7b7567d8601b110318ff5251c1ea65e75ade2b
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2017-01-14 00:03:41 +0100

    Añadida funcionalidad para asignar la posicion en funcion del tipo de pieza

--- carcassonne.py
+++ carcassonne.py
@@ -35,3 +35,49 @@ class Jugador:
         self.meeples = 8
         self.puntuacion = 0
 
+class Pieza_terreno:
+
+    def asignar_posicion(self, tipo):
+        if tipo == 1:
+            posicion = ['Granja','Granja','Camino','Camino']
+        elif tipo == 2:
+            posicion = ['Camino','Granja','Camino','Granja']
+        elif tipo == 3:
+            posicion = ['Castillo','Camino','Camino','Castillo']
+        elif tipo == 4:
+            posicion = ['Castillo','Granja','Granja','Castillo']
+        elif tipo == 5:
+            posicion = ['Granja','Castillo','Granja','Granja']
+        elif tipo == 6:
+            posicion = ['Castillo','Castillo','Granja','Castillo']
+        elif tipo == 7:
+            posicion = ['Granja','Camino','Camino','Camino']
+        elif tipo == 8:
+            posicion = ['Granja','Granja','Granja','Granja']
+        elif tipo == 9:
+            posicion = ['Granja','Castillo','Granja','Castillo']
+        elif tipo == 10:
+            posicion = ['Camino','Castillo','Camino','Granja']
+        elif tipo == 11:
+            posicion = ['Camino','Castillo','Camino','Camino']
+        elif tipo == 12:
+            posicion = ['Castillo','Castillo','Camino','Castillo']
+        elif tipo == 13:
+            posicion = ['Castillo','Camino','Camino','Granja']
+        elif tipo == 14:
+            posicion = ['Camino','Castillo','Granja','Camino']
+        elif tipo == 15:
+            posicion = ['Castillo','Granja','Castillo','Granja']
+        elif tipo == 16:
+            posicion = ['Granja','Castillo','Castillo','Granja']
+        elif tipo == 17:
+            posicion = ['Granja','Granja','Camino','Granja']
+        elif tipo == 18:
+            posicion = ['Camino','Camino','Camino','Camino']
+        elif tipo == 19:
+            posicion = ['Castillo','Castillo','Castillo','Castillo']
+        return posicion
+    
+    # Inicializa la clase pieza territorio
+    def __init__(self, tipo):
+        self.posicion = self.asignar_posicion(tipo)
