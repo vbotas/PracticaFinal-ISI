1bab5de81573b1ef66d6ce6be4f6a29d1599a83d
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2017-01-13 21:39:29 +0100

    Añadidos los tests

--- carcassonne_test.py
+++ carcassonne_test.py
@@ -23,5 +23,15 @@ class Test_basico(unittest.TestCase):
         expected = False
         self.assertEqual(expected, Partida().nombres_jug_correcto(jugadores))
 
+    def test_inicializar_jugador(self):
+        expected = 'Paco'
+        jugador1 = Jugador(expected)
+        self.assertEqual(expected, jugador1.nombre)
+
+    def test_inicializar_jugador2(self):
+        expected = 'Paco'
+        jugador2 = Jugador('Ana')
+        self.assertEqual(expected, jugador2.nombre)
+
 if __name__ == '__main__':
     unittest.main()
