ae6dc192d42f31a8652fb8220174f1c294f9d804
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2017-01-13 20:52:28 +0100

    Añadida funcionalidad

--- carcassonne.py
+++ carcassonne.py
@@ -1,6 +1,9 @@
+class Partida:
 
-
-class Partida(object):
-
-    def __init__(self, nombres_jugadores):
-        return "pasa"
+    def num_jug_correcto(self, nombres_jugadores):
+        num_maximo_jugadores = 4
+        num_jugadores = len(nombres_jugadores)
+        if num_jugadores > num_maximo_jugadores:
+            return False
+        else:
+            return True
--- carcassonne_test.py
+++ carcassonne_test.py
@@ -1,15 +1,17 @@
-import unittest
 from carcassonne import *
-
+import unittest
 
 class Test_basico(unittest.TestCase):
 
     def test_comprobar_numero_jugadores(self):
         jugadores = ['Paco','Ana']
         expected = True
-        self.assertEqual(expected, Partida.num_jug_correcto(jugadores))
+        self.assertEqual(expected, Partida().num_jug_correcto(jugadores))
 
-    def test_comprobar_numero_jugadores(self):
+    def test_comprobar_numero_jugadores2(self):
         jugadores = ['Paco','Ana','Maria','Jose','Sergio']
         expected = False
-        self.assertEqual(expected, Partida.num_jug_correcto(jugadores))
+        self.assertEqual(expected, Partida().num_jug_correcto(jugadores))
+
+if __name__ == '__main__':
+    unittest.main()
